8d6de53cbe3f578d38f625a69eababc714fdebc1
Lennon Spirlandelli <lennonpetrick@gmail.com>
2021-09-03 20:01:48 +0200

    Fixed the distance calculation and added the cases that used to fail to the unit test

--- app/src/main/java/com/example/airports/common/DistanceHelper.kt
+++ app/src/main/java/com/example/airports/common/DistanceHelper.kt
@@ -1,9 +1,7 @@
 package com.example.airports.common
 
 import javax.inject.Inject
-import kotlin.math.asin
-import kotlin.math.cos
-import kotlin.math.sqrt
+import kotlin.math.*
 
 /**
  * This calc is based on Haversine formula (https://en.wikipedia.org/wiki/Haversine_formula) and
@@ -14,14 +12,14 @@ internal class DistanceHelper @Inject constructor() {
 
     fun calculate(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
         val p = Math.PI / 180
-        val dLat = lat2 - lat1
-        val dLon = lon2 - lon1
-        val result = (0.5 - cos(dLat * p)) / 2 +
+        val dLat = (lat2 - lat1) * p
+        val dLon = (lon2 - lon1) * p
+        val result = sin(dLat / 2) * sin(dLat / 2) +
                 cos(lat1 * p) * cos(lat2 * p) *
-                (1 - cos(dLon * p)) / 2
+                sin(dLon / 2) * sin(dLon / 2)
 
         val earthDiameter = 12742 // Earth radius = 6371 km
-        return earthDiameter * asin(sqrt(result))
+        return earthDiameter * atan2(sqrt(result), sqrt(1 - result))
     }
 
 }
--- app/src/test/java/com/example/airports/common/DistanceHelperTest.kt
+++ app/src/test/java/com/example/airports/common/DistanceHelperTest.kt
@@ -31,23 +31,37 @@ internal class DistanceHelperTest {
             return Stream.of(
                 Arguments.of(
                     LocationsAndDistance(
+                        location1 = 45.733242 to 16.06152,
+                        location2 = 52.30907 to 4.763385,
+                        distance = 1099.2832728616806
+                    )
+                ),
+                Arguments.of(
+                    LocationsAndDistance(
+                        location1 = 25.248665 to 55.352917,
+                        location2 = 52.30907 to 4.763385,
+                        distance = 5168.280571167744
+                    )
+                ),
+                Arguments.of(
+                    LocationsAndDistance(
                         location1 = 57.08655 to 9.872241,
                         location2 = 35.049625 to -106.617195,
-                        distance = 4269.838770984859
+                        distance = 8174.9357599374625
                     )
                 ),
                 Arguments.of(
                     LocationsAndDistance(
                         location1 = 33.640068 to -84.44403,
                         location2 = 47.433037 to 19.261621,
-                        distance = 4362.706510579426
+                        distance = 8235.558011944086
                     )
                 ),
                 Arguments.of(
                     LocationsAndDistance(
                         location1 = 32.837223 to -115.57472,
                         location2 = 40.904675 to 29.309189,
-                        distance = 7826.990893695751
+                        distance = 11059.589826737658
                     )
                 ),
             )
