9a5bc6bf8d94a41965add3a091c0fd644f298add
Lennon Spirlandelli <lennonpetrick@gmail.com>
2021-09-08 17:41:54 +0200

    Implemented AirportListScreen to show the list

--- app/src/main/java/com/example/airports/di/DaggerViewModelExtension.kt
+++ app/src/main/java/com/example/airports/di/DaggerViewModelExtension.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.airports.presentation.ComposeLifecycleObserver
 
 @Composable
 inline fun <reified T : ViewModel> daggerViewModel(
@@ -13,7 +14,11 @@ inline fun <reified T : ViewModel> daggerViewModel(
     factory = object : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             @Suppress("UNCHECKED_CAST")
-            return viewModelCreator() as T
+            return viewModelCreator().apply {
+                if (this is ComposeLifecycleObserver) {
+                    this.onCreate()
+                }
+            } as T
         }
     }
 )
--- app/src/main/java/com/example/airports/presentation/airportlist/AirportListScreen.kt
+++ app/src/main/java/com/example/airports/presentation/airportlist/AirportListScreen.kt
@@ -1,8 +1,24 @@
 package com.example.airports.presentation.airportlist
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.airports.R
 import com.example.airports.di.daggerViewModel
+import com.example.airports.presentation.DisplayText
+import com.example.airports.presentation.components.GenericError
 
 @Composable
 internal fun AirportListScreen(
@@ -10,5 +26,94 @@ internal fun AirportListScreen(
         AirportListComponent.getViewModel()
     }
 ) {
-    val viewState = viewModel.state.observeAsState()
+    val state by viewModel.state<AirportListState>().observeAsState()
+    state?.apply {
+        when (this) {
+            is AirportListState.Loading -> Loading()
+            is AirportListState.Loaded -> AirportList(airportList)
+            is AirportListState.Error -> GenericError()
+        }
+    }
 }
+
+@Composable
+internal fun Loading() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+internal fun AirportList(items: List<AirportListView>) {
+    LazyColumn(Modifier.fillMaxSize()) {
+        items(items) {
+            AirportListItem(it)
+        }
+    }
+}
+
+@Composable
+internal fun AirportListItem(view: AirportListView) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        ItemText(
+            text = view.name,
+            modifier = Modifier.weight(1f),
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Spacer(Modifier.width(10.dp))
+
+        ItemText(
+            text = view.distance.let { stringResource(it.text, it.argValue!!) }
+        )
+    }
+}
+
+@Composable
+internal fun ItemText(
+    text: String,
+    modifier: Modifier = Modifier,
+    overflow: TextOverflow = TextOverflow.Clip
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        style = MaterialTheme.typography.subtitle1,
+        maxLines = 1,
+        overflow = overflow
+    )
+}
+
+@Preview
+@Composable
+internal fun LoadingPreview() {
+    Loading()
+}
+
+@Preview
+@Composable
+internal fun AirportListPreview() {
+    val list = (1..5).map { createFakeAirportListView(it) }
+    AirportList(list)
+}
+
+@Preview
+@Composable
+internal fun AirportListItemPreview() {
+    AirportListItem(createFakeAirportListView())
+}
+
+private fun createFakeAirportListView(id: Int = 1) = AirportListView(
+    id = id.toString(),
+    name = "Airport $id",
+    distance = DisplayText(R.string.distance_km, id*id)
+)
