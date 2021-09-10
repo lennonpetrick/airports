15b31b62d9be239b2ab6868fd2b8d4b6ef4fd0ae
Lennon Spirlandelli <lennonpetrick@gmail.com>
2021-09-10 15:33:42 +0200

    Implemented details screen

--- app/src/main/java/com/example/airports/presentation/DisplayText.kt
+++ app/src/main/java/com/example/airports/presentation/DisplayText.kt
@@ -2,4 +2,4 @@ package com.example.airports.presentation
 
 import androidx.annotation.StringRes
 
-internal class DisplayText(@StringRes val text: Int, val argValue: Any?)
+internal class DisplayText(@StringRes val text: Int, val argValue: Any)
--- app/src/main/java/com/example/airports/presentation/airportdetails/AirportDetailsActivity.kt
+++ app/src/main/java/com/example/airports/presentation/airportdetails/AirportDetailsActivity.kt
@@ -5,12 +5,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.airports.R
 import com.example.airports.di.daggerViewModel
 import com.example.airports.presentation.DisplayText
@@ -66,7 +74,82 @@ class AirportDetailsActivity : AppCompatActivity() {
 
     @Composable
     private fun AirportDetails(details: AirportDetailsView) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            Column(
+                Modifier
+                    .padding(16.dp)
+            ) {
+                RegularText(text = "${details.id} - ${details.city}, ${details.countryId}")
+                RegularSpace()
 
+                RegularText(text = details.name)
+                SmallSpace()
+                SmallText(text = stringResource(R.string.airport_details_location, details.latitude, details.longitude))
+
+                details.nearestAirport?.apply {
+                    LargeSpace()
+                    Divider()
+                    RegularSpace()
+
+                    LargeText(stringResource(R.string.airport_details_nearest))
+                    RegularSpace()
+
+                    RegularText(text = name)
+                    SmallSpace()
+                    SmallText(text = getDistanceText(distance, details.name))
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun LargeSpace() {
+        Spacer(modifier = Modifier.height(30.dp))
+    }
+
+    @Composable
+    private fun RegularSpace() {
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+
+    @Composable
+    private fun SmallSpace() {
+        Spacer(modifier = Modifier.height(3.dp))
+    }
+
+    @Composable
+    private fun LargeText(text: String) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.h5,
+        )
+    }
+
+    @Composable
+    private fun RegularText(text: String) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.subtitle1,
+        )
+    }
+
+    @Composable
+    private fun SmallText(text: String) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.subtitle2,
+            color = Color.Gray
+        )
+    }
+
+    @Composable
+    private fun getDistanceText(displayText: DisplayText, fromAirportName: String): String {
+        val distanceText = displayText.let { stringResource(it.text, it.argValue) }
+        return stringResource(R.string.airport_details_nearest_distance, distanceText, fromAirportName)
     }
 
     @Preview
